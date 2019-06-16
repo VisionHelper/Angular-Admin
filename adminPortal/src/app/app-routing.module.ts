fb3778c1f256aa5bb7514deee603b1bc9f439a96
Nivas\Acer <mane.nivas4@gmail>
2019-06-16 15:59:14 +0530

    Jobseeker And Employer form

--- adminPortal/src/app/app-routing.module.ts
+++ adminPortal/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { CategoryComponent } from './category/category.component';
+import { JobSeekerComponent } from './job-seeker/job-seeker.component';
+import { EmployerComponent } from './employer/employer.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'category', component: CategoryComponent }
+  { path: 'category', component: CategoryComponent },
+  { path: 'jobSeeker', component: JobSeekerComponent },
+  { path: 'employer', component: EmployerComponent }
 ];
 
 @NgModule({
---
+++ adminPortal/src/app/employer/employer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employer',
+  templateUrl: './employer.component.html',
+  styleUrls: ['./employer.component.css']
+})
+export class EmployerComponent implements OnInit {
+
+  pageSection : string;
+  employer = {}
+  constructor() { }
+
+  ngOnInit() {
+    this.pageSection ='list';
+  }
+
+}
